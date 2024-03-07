1ee00b9ba526150184eb3a39b541b716b17571e1
Shifting5164 <16780030+Shifting5164@users.noreply.github.com>
2024-03-07 20:55:46 +0100

    Add IPC between master and workers (#11)
    * [ADD] ipc
    
    * [FIX] code check
    
    ---------
    
    Co-authored-by: user <user@ubuntu.myguest.virtualbox.org>

--- include/cew_exit.h
+++ include/cew_exit.h
@@ -2,11 +2,12 @@
 #define CEWSERVER_CEW_EXIT_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 extern bool bTerminateProg;
 
-void do_exit(const int32_t);
+void do_exit(int32_t);
 
-void do_exit_with_errno(const int32_t);
+void do_exit_with_errno(int32_t);
 
 #endif //CEWSERVER_CEW_EXIT_H
